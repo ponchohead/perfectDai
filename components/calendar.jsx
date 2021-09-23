344cfc377596ee2e7443d5ddd4cbba6b2a16fec9
ponchohead <alzamudioglez@gmail.com>
2021-09-23 10:11:58 -0500

    DnDcalendar change

--- components/calendar.jsx
+++ components/calendar.jsx
@@ -1,30 +1,43 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
 
 const myEventsList = [
     {
       title: "today",
-      start: new Date("2019-05-05 10:22:00"),
-      end: new Date("2019-05-05 10:42:00"),
+      start: new Date("2021-09-05 10:22:00"),
+      end: new Date("2021-09-05 10:42:00"),
     },
     {
-      title: "string",
-      start: new Date("2019-05-05 12:22:00"),
-      end: new Date("2019-05-05 13:42:00"),
+      title: "Viva México",
+      start: new Date("2021-09-15 12:22:00"),
+      end: new Date("2021-09-15 13:42:00"),
     },
   ]
 
 export const MyCalendar = props => (
   <div>
-    <Calendar
+    {/* <Calendar
       localizer={localizer}
       events={myEventsList}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500 }}
-    />
+      style={{ height: 600 }}
+    /> */}
+
+    <DnDCalendar
+      defaultDate={moment().toDate()}
+      defaultView="month"
+      events={myEventsList}
+      localizer={localizer}
+      resizable
+      style={{ height: "100vh" }}
+    />    
+
+   
   </div>
 )
